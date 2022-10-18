19f186b795907c22b659a2168a4906e0cf8408d2
GeorgeCacoulidis <georgec@protonmail.com>
2022-10-17 23:44:31 -0400

    corrected R00_IDS16_J

--- R00_IDS16_J.java
+++ R00_IDS16_J.java
@@ -4,11 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
  
 public class OnlineStore {
-  private static void createXMLStreamBad(final BufferedOutputStream outStream,
-      final String quantity) throws IOException {
+  private static void createXMLStream(final BufferedOutputStream outStream,
+      final String quantity) throws IOException, NumberFormatException {
+    // Write XML string only if quantity is an unsigned integer (count).
+    int count = Integer.parseUnsignedInt(quantity);
     String xmlString = "<item>\n<description>Widget</description>\n"
-        + "<price>500</price>\n" + "<quantity>" + quantity
-        + "</quantity></item>";
+        + "<price>500</price>\n" + "<quantity>" + count + "</quantity></item>";
     outStream.write(xmlString.getBytes());
     outStream.flush();
   }
