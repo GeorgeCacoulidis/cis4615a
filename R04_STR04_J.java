d729a489c544faf2ad23c3072edee7daaa3bd7e0
GeorgeCacoulidis <georgec@protonmail.com>
2022-10-17 23:48:45 -0400

    corrected R04_STR04_J

--- R04_STR04_J.java
+++ R04_STR04_J.java
@@ -1,11 +1,10 @@
-
 FileInputStream fis = null;
 try {
   fis = new FileInputStream("SomeFile");
   DataInputStream dis = new DataInputStream(fis);
   byte[] data = new byte[1024];
   dis.readFully(data);
-  String result = new String(data);
+  String result = new String(data, "UTF-16LE");
 } catch (IOException x) {
   // Handle error
 } finally {
